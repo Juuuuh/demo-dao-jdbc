77e551dddbf445f59dd07841bc829d31bd7b1166
Juuuuh <ajuliagomes45@gmail.com>
2026-02-24 12:59:40 +0000

    findById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -95,8 +95,8 @@ public class Seller implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
-				+ baseSalary + ", departent=" + departent + "]";
+		return "Seller [id= " + id + ", name= " + name + ", email= " + email + ", birthDate= " + birthDate + ", baseSalary= "
+				+ baseSalary + ", departent= " + departent + "]";
 	}
 	
 	
