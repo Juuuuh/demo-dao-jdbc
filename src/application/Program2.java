56c28cbbefecd96adad799420c8e55a5c2c6695b
Juuuuh <ajuliagomes45@gmail.com>
2026-02-25 11:58:49 +0000

    delete Department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -34,6 +34,14 @@ public class Program2 {
 		departmentDao.update(department);
 		System.out.println("Update completed!");
 		
+		System.out.println("\n=== TEST 5: seller delete ===");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
+		
 
 	}
 
